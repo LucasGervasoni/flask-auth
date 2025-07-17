1de3a8a0045674f674b87a5f1f58bef40b3143db
Lucas Gervasoni <jaxellucas@gmail.com>
2025-07-17 08:43:10 -0300

    feat: adicionando o código de logout

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user,login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -18,6 +18,7 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(user_id)
 
+#login
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
@@ -25,14 +26,20 @@ def login():
     password = data.get('password')
     
     if username and password:
-        #login
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
             login_user(user)
-            print(current_user.is_authenticated)
             return jsonify({'message': 'Login successful'}), 200      
     
     return jsonify({'message': 'Invalid credentials'}), 401
+
+#logout
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({'message': 'Logout successful'})
+    
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
