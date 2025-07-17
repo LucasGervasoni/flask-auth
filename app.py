9184ecfd49b9734e422a7e5b07759091bbadd696
Lucas Gervasoni <jaxellucas@gmail.com>
2025-07-17 08:03:07 -0300

    feat: adicionando a view de login e criando o código de autenticação do usuario

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.user import User
 from database import db
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+#view login
+login_manager.login_view = 'login'
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    
+    if username and password:
+        #login
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({'message': 'Login successful'}), 200      
+    
+    return jsonify({'message': 'Invalid credentials'}), 401
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
